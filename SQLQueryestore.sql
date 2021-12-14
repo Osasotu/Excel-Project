c6458661a514aa1e5e4dccfaeeefc65bced63af9
Osarense Edomwonyi-Otu <95641932+Osasotu@users.noreply.github.com>
2021-12-13 17:01:47 -0700

    Add files via upload

---
+++ SQLQueryestore.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE db_estore
+USE db_estore
+CREATE TABLE suppliers (
+suppliers_id INT PRIMARY KEY NOT NULL,
+suppliers_name VARCHAR(250) NOT NULL,
+suppliers_address VARCHAR(250) NOT NULL,
+addressline2 VARCHAR(250) NULL,
+postal_code CHAR(6) NOT NULL,
+country VARCHAR(25) NOT NULL,
+phone_number INT NOT NULL,
+fax INT NULL
+);
+CREATE TABLE products (
+product_id INT PRIMARY KEY NOT NULL,
+product_name VARCHAR(50) NOT NULL,
+product_quatity INT NOT NULL,
+suppliers_id INT NOT NULL
+);
+CREATE TABLE shipping (
+shippers_id INT PRIMARY KEY NOT NULL,
+shippers_name VARCHAR(250) NOT NULL,
+shippers_address VARCHAR(250) NOT NULL,
+addressline2 VARCHAR(250) NULL,
+postal_code CHAR(6) NOT NULL,
+country VARCHAR(250) NOT NULL,
+phone_number INT NOT NULL,
+product_id INT NOT NULL, 
+suppLiers_id INT NOT NULL
+);
